5efde8c9997b311096eb848ac25d4e2b87685313
MdMusthaqAsim <musthaqasim.shaik@gmail.com>
2025-06-10 20:03:12 +0530

    Initial commit

---
+++ src/main/java/org/hoyo/celestia/CelestiaApplication.java
@@ -0,0 +1,13 @@
+package org.hoyo.celestia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelestiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CelestiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hoyo/celestia/CelestiaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hoyo.celestia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelestiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
