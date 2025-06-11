a1cefdd78c0644be04b0e558baffd4dacf472d64
MdMusthaqAsim <musthaqasim.shaik@gmail.com>
2025-06-11 22:37:08 +0530

    user init

--- src/main/java/org/hoyo/celestia/user/UserController.java
+++ src/main/java/org/hoyo/celestia/user/UserController.java
@@ -4,9 +4,11 @@ import org.hoyo.celestia.user.service.CreateUserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     private final CreateUserService createUserService;
@@ -16,7 +18,7 @@ public class UserController {
     }
 
     @GetMapping("/{uid}")
-    public void createUser(@PathVariable String uid){
-        createUserService.fetchUser(uid);
+    public ResponseEntity<String> createUser(@PathVariable String uid){
+        return createUserService.upsertUser(uid);
     }
 }
