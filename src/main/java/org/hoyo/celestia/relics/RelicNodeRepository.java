b6ded242022bb75499502c4074ad27cf454e95a9
MdMusthaqAsim <musthaqasim.shaik@gmail.com>
2025-06-26 16:43:16 +0530

    relic insertion code

--- src/main/java/org/hoyo/celestia/relics/RelicNodeRepository.java
+++ src/main/java/org/hoyo/celestia/relics/RelicNodeRepository.java
@@ -1,18 +1,63 @@
 package org.hoyo.celestia.relics;
 
 import org.hoyo.celestia.relics.model.RelicNode;
+import org.hoyo.celestia.relics.model.SubAffixNode;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public interface RelicNodeRepository extends Neo4jRepository<RelicNode, Long> {
 
     @Query("""
-            MERGE (r:RelicNode {
-                mainAffixId: $relicNode.mainAffixId
-            })
-            return r
+    CREATE (r:RelicNode {
+        relicId: $relicId,
+        mainAffixId: $mainAffixId,
+        tid: $tid,
+        type: $type,
+        level: $level,
+        setId: $setId,
+        setName: $setName,
+        mainType: $mainType,
+        mainValue: $mainValue
+    })
+            
+    WITH r
+    MATCH (u:UIDNode {uid: $uid})
+    CREATE (u)-[:RELIC]->(r)
+            
+    WITH r, $subAffixes AS subAffixes
+    UNWIND subAffixes AS sa
+    CREATE (s:SubAffixNode {
+        type: sa.type,
+        value: sa.value,
+        cnt: sa.cnt,
+        step: sa.step
+    })
+    CREATE (r)-[:SUBAFFIX]->(s)
+            
+    RETURN DISTINCT r
+            
+""")
+    RelicNode insertRelic(
+            String relicId,
+            String uid,
+            String mainAffixId,
+            String tid,
+            String type,
+            String level,
+            String setId,
+            String setName,
+            String mainType,
+            Double mainValue,
+            List<Map<String, Object>> subAffixes
+    );
+
+    @Query("""
+            RETURN EXISTS( (:UIDNode {uid: $uid})-[:RELIC]->(:RelicNode {relicId: $relicId}) )
             """)
-    public RelicNode linkRelicNodeToStore(String relicId, String uid, RelicNode relicNode);
+    Boolean existsRelic(String relicId, String uid);
 }
