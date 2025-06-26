908a4a9bbb86e3b953a2af9fb595346baa31dedd
MdMusthaqAsim <musthaqasim.shaik@gmail.com>
2025-06-26 15:47:43 +0530

    hehehe

--- src/main/java/org/hoyo/celestia/relics/RelicNodeRepository.java
+++ src/main/java/org/hoyo/celestia/relics/RelicNodeRepository.java
@@ -2,9 +2,17 @@ package org.hoyo.celestia.relics;
 
 import org.hoyo.celestia.relics.model.RelicNode;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RelicNodeRepository extends Neo4jRepository<RelicNode, Long> {
 
+    @Query("""
+            MERGE (r:RelicNode {
+                mainAffixId: $relicNode.mainAffixId
+            })
+            return r
+            """)
+    public RelicNode linkRelicNodeToStore(String relicId, String uid, RelicNode relicNode);
 }
--- src/main/java/org/hoyo/celestia/relics/service/CreateRelicService.java
+++ src/main/java/org/hoyo/celestia/relics/service/CreateRelicService.java
@@ -1,14 +1,26 @@
 package org.hoyo.celestia.relics.service;
 
+import org.hoyo.celestia.relics.RelicNodeRepository;
+import org.hoyo.celestia.relics.SubAffixNodeRepository;
 import org.hoyo.celestia.relics.model.RelicNode;
+import org.hoyo.celestia.relics.model.SubAffixNode;
 import org.hoyo.celestia.user.model.Relic;
 import org.hoyo.celestia.user.model.SubAffix;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CreateRelicService {
+
+    private final RelicNodeRepository relicNodeRepository;
+    private final SubAffixNodeRepository subAffixNodeRepository;
+
+    public CreateRelicService(RelicNodeRepository relicNodeRepository, SubAffixNodeRepository subAffixNodeRepository) {
+        this.relicNodeRepository = relicNodeRepository;
+        this.subAffixNodeRepository = subAffixNodeRepository;
+    }
 
     public String calculateRelicId(Relic relic){
         try {
@@ -27,8 +39,39 @@ public class CreateRelicService {
         }
     }
 
-    public RelicNode createRelic(Relic relic){
+    public String createRelicNode(Relic relic){
+        RelicNode relicNode = new RelicNode();
+        relicNode.setRelicId(calculateRelicId(relic));
+        relicNode.setMainAffixId(String.valueOf(relic.getMainAffixId()));
+        relicNode.setTid(relic.getTid());
+        relicNode.setType(String.valueOf(relic.getType()));
+        relicNode.setLevel(String.valueOf(relic.getLevel()));
+        relicNode.setSetId(String.valueOf(relic.get_flat().getSetID()));
+        relicNode.setSetName(relic.get_flat().getSetName());
+        relicNode.setMainType(relic.get_flat().getProps().getFirst().getType());
+        relicNode.setMainValue(relic.get_flat().getProps().getFirst().getValue());
 
-        return null;
+        List<SubAffixNode> subAffixNodes = new ArrayList<>();
+        int count = 1;
+        for(SubAffix subAffix : relic.getSubAffixList()){
+            SubAffixNode subAffixNode = new SubAffixNode();
+            subAffixNode.setStep(subAffix.getStep());
+            subAffixNode.setCnt(subAffix.getCnt());
+            subAffixNode.setType(relic.get_flat().getProps().get(count).getType());
+            subAffixNode.setValue(relic.get_flat().getProps().get(count).getValue());
+            subAffixNodes.add(subAffixNode);
+            count++;
+        }
+
+        relicNodeRepository.save(relicNode);
+        //link it to relicStore under uid
+
+        for(SubAffixNode subAffixNode : subAffixNodes){
+            subAffixNodeRepository.save(subAffixNode);
+            //link it to relicNode from above
+        }
+
+
+        return true;
     }
 }
