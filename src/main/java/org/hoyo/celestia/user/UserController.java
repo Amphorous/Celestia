4679c895bbdb20ec197ddcdeea3c491c2a3658d6
MdMusthaqAsim <musthaqasim.shaik@gmail.com>
2025-08-03 22:14:56 +0530

    back end to front end first POC
    norefresh endpoint added

--- src/main/java/org/hoyo/celestia/user/UserController.java
+++ src/main/java/org/hoyo/celestia/user/UserController.java
@@ -1,24 +1,31 @@
 package org.hoyo.celestia.user;
 
+import org.hoyo.celestia.user.DTOs.NoRefreshUserDTO;
 import org.hoyo.celestia.user.service.CreateUserService;
+import org.hoyo.celestia.user.service.UserDetailsFetchService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     private final CreateUserService createUserService;
+    private final UserDetailsFetchService userDetailsFetchService;
 
-    public UserController(CreateUserService createUserService) {
+    public UserController(CreateUserService createUserService, UserDetailsFetchService userDetailsFetchService) {
         this.createUserService = createUserService;
+        this.userDetailsFetchService = userDetailsFetchService;
     }
 
     @GetMapping("/{uid}")
     public ResponseEntity<String> createUser(@PathVariable String uid){
         return createUserService.upsertUser(uid);
+    }
+
+    @GetMapping("/dashboard/noRefresh/{uid}")
+    public ResponseEntity<NoRefreshUserDTO> refreshUser(@PathVariable String uid){
+        return userDetailsFetchService.getUserCardDetailsNoRefresh(uid);
     }
 }
---
+++ src/main/java/org/hoyo/celestia/user/service/UserDetailsFetchService.java
@@ -0,0 +1,34 @@
+package org.hoyo.celestia.user.service;
+
+import org.hoyo.celestia.user.DTOs.NoRefreshUserDTO;
+import org.hoyo.celestia.user.model.User;
+import org.hoyo.celestia.user.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsFetchService {
+
+    private final UserRepository userRepository;
+    private final CreateUserService createUserService;
+
+    public UserDetailsFetchService(UserRepository userRepository, CreateUserService createUserService) {
+        this.userRepository = userRepository;
+        this.createUserService = createUserService;
+    }
+
+    //this refreshes if the user isnt in the DB
+    public ResponseEntity<NoRefreshUserDTO> getUserCardDetailsNoRefresh(String uid){
+
+        if(!userRepository.existsById(uid)){
+            User user = createUserService.getUser(uid);
+            NoRefreshUserDTO noRefreshUserDTO = new NoRefreshUserDTO(user);
+            noRefreshUserDTO.setRegion("NONE");
+            return ResponseEntity.ok(noRefreshUserDTO);
+        }
+
+        NoRefreshUserDTO noRefreshUserDTO = userRepository.findUserCardByUid(uid);
+        noRefreshUserDTO.setRegion("NONE");
+        return ResponseEntity.ok(noRefreshUserDTO);
+    }
+}
